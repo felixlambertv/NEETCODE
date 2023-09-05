f312360264ead0889f3a13d68b28afbf20b41a03
Felix Lambert <felix@itsavirus.com>
2023-09-05 22:23:33 +0800

    Add top k frequest test

---
+++ top_k_frequest_test.go
@@ -0,0 +1,65 @@
+package neetcode
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"testing"
+)
+
+func topKFrequent(nums []int, k int) []int {
+	m := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		m[nums[i]] += 1
+	}
+
+	type kv struct {
+		Key int
+		Val int
+	}
+
+	var keys []kv
+	for key, value := range m {
+		keys = append(keys, kv{key, value})
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i].Val > keys[j].Val
+	})
+
+	var res []int
+	for _, key := range keys {
+		res = append(res, key.Key)
+		if len(res) == k {
+			return res
+		}
+	}
+
+	return []int{}
+}
+
+func TestTopKFrequent(t *testing.T) {
+	testCases := []struct {
+		nums     []int
+		k        int
+		expected []int
+	}{
+		{[]int{1, 2}, 2, []int{1, 2}},
+		{[]int{1, 1, 1, 2, 2, 3}, 2, []int{1, 2}},
+		{[]int{4, 1, 4, 3, 4, 3, 2, 1, 2}, 3, []int{4, 1, 3}},
+		{[]int{5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, 1, []int{5}},
+		{[]int{1, 2, 3, 4, 5}, 5, []int{1, 2, 3, 4, 5}},
+		{[]int{1, 2, 3, 4, 5}, 0, []int{}}, // Edge case: k is 0
+		{[]int{}, 3, []int{}},              // Edge case: empty input slice
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("TopKFrequent(%v, %d)", tc.nums, tc.k), func(t *testing.T) {
+			res := topKFrequent(tc.nums, tc.k)
+			if !reflect.DeepEqual(tc.expected, res) {
+				t.Errorf("Expected %v, but got %v", tc.expected, res)
+			}
+		})
+	}
+}
